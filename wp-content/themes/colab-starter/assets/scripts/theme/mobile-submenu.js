31fd9172be5903f0d192dd204ad3234e226bf458
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-04-20 17:52:50 -0400

    mobile sub menu and dist

--- wp-content/themes/colab-starter/assets/scripts/theme/mobile-submenu.js
+++ wp-content/themes/colab-starter/assets/scripts/theme/mobile-submenu.js
@@ -1,13 +1,27 @@
 function init() {
+    const subexpand = document.querySelectorAll(".menu-expand");
 
-    const subexpand = document.querySelectorAll('.menu-expand');
+    const openSubMenu = (parent) => {
+        parent.classList.add("expanded", "visible");
+    };
 
-subexpand.forEach(sub => {
-    sub.addEventListener('click', (event) => {
-    let parent = event.target.closest('.menu-item-has-children');
-    parent.classList.toggle('expanded');
-});
-});
+    const closeSubMenu = (parent) => {
+        parent.classList.remove("expanded");
+        setTimeout(() => {
+            parent.classList.remove("visible");
+        }, 500);
+    };
+
+    subexpand.forEach((sub) => {
+        sub.addEventListener("click", (event) => {
+            let parent = event.target.closest(".menu-item-has-children");
+            if (parent.classList.contains("expanded")) {
+                closeSubMenu(parent);
+            } else {
+                openSubMenu(parent);
+            }
+        });
+    });
 }
 
 export { init as default };
